4b37c4c1c885ab1e9dea85f46b0209a138bd70ea
achunpk <aswathi.npk@gmail.com>
2019-11-15 00:48:27 -0400

    Updated facebook code

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:/Users/Mac/Cydorg-automation/cucumber/src/main/java/Features"
+        features = {"C:/Users/Mac/Cydorg-automation/cucumber/src/main/java/Features"}
         ,glue={"stepDefinition"},
         tags={"@facebook"}
 
